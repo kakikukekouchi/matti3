736100e60f900aa9f3b9b495076e19c2b17df00f
kakikukekouchi <138739444+kakikukekouchi@users.noreply.github.com>
2023-07-14 12:08:54 +0900

    ヘッダファイルに追加した
    昨日の続き

--- matti3/matti3/AppMain.cpp
+++ matti3/matti3/AppMain.cpp
@@ -56,13 +56,13 @@
 
 ******************************************/
 
-int WINAPI WinMain(_In_HINSTANCE hinstance, _In_opt_HINSTANCE hPrevInstace, _In_LPSTR lpCmdLine, _In_int nShowCmd)
+int WINAPI WinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hPrevInstace, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 
 	//ウィンドウタイトル設定
 	SetMainWindowText("Match 3 Puzzle");
 	//ウィンドモードで起動
-	ChangWindowTMode(TRUE);
+	ChangeWindowMode(TRUE);
 	//画面サイズの最大サイズ,カラービット数を設定
 	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
 
@@ -79,6 +79,47 @@ int WINAPI WinMain(_In_HINSTANCE hinstance, _In_opt_HINSTANCE hPrevInstace, _In_
 
 	Input_Initialize();          //入力制限機能
 
+
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+
+		//入力制御機能更新処理
+		Input_Update();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreamControl_Update();
+
+		//画面の内容を表画面に反映
+		ScreenFlip();
+
+	}
+	//Dxライブラリ使用の終了処理
+	Dxlib_End();
+
+	return 0;
 
 }
 
